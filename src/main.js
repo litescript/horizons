31087418db45f49931236550b321ffd4d6dee7cd
Peter Brown <brown.peterg@gmail.com>
2026-08-02 12:18:06 -0400

    add sun sphere to scene, rotate

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import * as THREE from 'three';
 
+///////////////////////////////////////////////////////////////////////
+//    dev toggle //////////////////////////////////////////////////////
+const isDev = true;
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
 // oh, hello
 console.log('Horizons initialized.');
 
 // variables, DOM
 const sceneCanvas = document.getElementById('scene-canvas');
+const splashScreen = document.getElementById('splash');
 sceneCanvas.width = window.innerWidth;
 sceneCanvas.height = window.innerHeight;
 const aspectRatio = sceneCanvas.width / sceneCanvas.height;
@@ -14,13 +21,15 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera (
   50,             // FOV (def: 45)
   aspectRatio,    // aspect ratio
-  0.1,              // near plane (def: 0.1)
+  0.1,            // near plane (def: 0.1)
   2000,           // far plane (def: 2000)
 );
 const renderer = new THREE.WebGLRenderer({
+  canvas: sceneCanvas,
   antialias: true,
 });
-renderer.setSize(sceneCanvas.width, sceneCanvas.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 // the sun
 const sunGeometry = new THREE.SphereGeometry(
@@ -35,7 +44,20 @@ const sun = new THREE.Mesh(sunGeometry, sunMaterial);
 
 // paint the scene
 scene.add(sun);
-camera.position.z = 5;
+camera.position.z = 40;
+
+function resizeScene() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setSize(width, height, false);
+}
+
+window.addEventListener('resize', resizeScene);
 
 // animation loop
 function animate(time) {
@@ -45,5 +67,13 @@ function animate(time) {
   renderer.render(scene, camera);
 }
 
+if (isDev) {
+  splashScreen.style.display = 'none';
+  sceneCanvas.style.display = 'block';
+  sceneCanvas.style.zIndex = 1;
+  sceneCanvas.style.visibility = 'visible';
+}
+
+resizeScene();
 renderer.setAnimationLoop(animate);
 
